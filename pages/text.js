1e5a8f7515bc213044bc56ee4073bf6648831d52
unknown <ckdu22.5.9@hotmail.com>
2022-10-09 15:44:25 +0700

    deploy

--- pages/text.js
+++ pages/text.js
@@ -14,6 +14,7 @@ import styles from '../styles/feature.module.scss';
 import Stack from '@mui/material/Stack';
 import Image from 'next/image';
 import { Link, animateScroll as scroll } from "react-scroll";
+import { useEffect } from 'react';
 
 export default function Home() {
   const scrollToTop = () => {
@@ -22,16 +23,19 @@ export default function Home() {
   const scrollToTopTwo = () => {
     scroll.scrollTo(1520);
   };
+  useEffect(()=>{
+    scroll.scrollTo(15);
+  })
   return (
     <Container  >
       <div className={stylesd.setDisplay}>
-        <h1 style={{ textAlign: 'left', color: '#744a2d', fontSize: '60px', transform: 'translate(40px,-50px)', fontWeight: 400 }}>
+        <h1 style={{ textAlign: 'left', color: '#744a2d', fontSize: '60px', transform: 'translate(65px,-50px)', fontWeight: 400 }}>
           อาหารฮาลาน</h1>
-        <h1 style={{ textAlign: 'left', color: '#018a42', fontWeight: 400, fontSize: '60px', transform: 'translate(40px,-110px)' }}>ลดโลกร้อน</h1>
-        <h1 style={{ textAlign: 'left', color: '#212120', fontWeight: 'bold', fontSize: '18px', transform: 'translate(40px,-150px)' }}>
+        <h1 style={{ textAlign: 'left', color: '#018a42', fontWeight: 400, fontSize: '60px', transform: 'translate(65px,-110px)' }}>ลดโลกร้อน</h1>
+        <h1 style={{ textAlign: 'left', color: '#212120', fontWeight: 'bold', fontSize: '18px', transform: 'translate(65px,-150px)' }}>
           โครงการส่งเสริมภาพลักษณ์ของผลิต<br />ภัณฑ์อาหาร HALAL เพื่อการ<br />ท่องเที่ยว 5 จังหวัดชายแดนภาคใต้
         </h1>
-        <div style={{ transform: 'translate(45px,-140px)' }}>
+        <div style={{ transform: 'translate(70px,-140px)' }}>
           <Stack direction="row" spacing={2}>
             <div>
               <Button
@@ -56,54 +60,64 @@ export default function Home() {
 
             </div>
           </Stack>
-          <div className={styles['x_feature'] + " " + stylesd['setDisplays']}  id="x_feature" style={{transform:'translate(0px,-10px)'}} >
-          <p style={{ fontSize: '18px', fontWeight: 500 }}>สนับสนุน โดย</p>
-          <div className={styles['zone-mobile']}>
-            <ul className={styles['zone-logo']}>
-              <li
-                data-aos="zoom-in-up"
-                data-aos-duration="600"
-                data-aos-easing="ease-in-out"
-                data-aos-once="true">
-                <div className={styles['img-logotwo'] + ' ' + 'm-auto'}>
-                  <Image src={Ic1} alt="" />
-                </div>
-              </li>
-              <li
-                data-aos="zoom-in-up"
-                data-aos-duration="700"
-                data-aos-easing="ease-in-out"
-                data-aos-once="true">
-                <div className={styles['img-logotwo'] + ' ' + 'm-auto'} style={{ transform: 'translate(0px,10px)' }}>
-                  <Image src={Ic2} alt="" />
-                </div>
-              </li>
-              <li
-                data-aos="zoom-in-up"
-                data-aos-duration="800"
-                data-aos-easing="ease-in-out"
-                data-aos-once="true">
-                <div className={styles['img-logotwo'] + ' ' + 'm-auto'} style={{ transform: 'translate(0px,10px)' }}>
-                  <Image src={Ic3} alt="" />
-                </div>
-              </li>
-              <li
-                data-aos="zoom-in-up"
-                data-aos-duration="900"
-                data-aos-easing="ease-in-out"
-                data-aos-once="true">
-                <div className={styles['img-logotwo'] + ' ' + 'm-auto'}>
-                  <Image src={Ic4} alt="" />
-                </div>
-                <div style={{ transform: 'translate(0px,-15px)' }}>
-                  <small >ม.ราชภัฎสงขลา<br />ม.ราชภัฎยะลา</small>
-                </div>
-              </li>
-            </ul>
+          <div className={styles['x_feature'] + " " + stylesd['setDisplays']} id="x_feature" style={{ transform: 'translate(-50px,-10px)' }} >
+            <div
+        data-aos="zoom-in-up"
+        data-aos-duration="500"
+        data-aos-easing="ease-in-out"
+        data-aos-once="true"
+              >
+
+              <div className={styles['zone-logo']}>
+                <p style={{ fontSize: '18px', fontWeight: 500 }}>สนับสนุน โดย</p>
+              </div>
+            </div>
+            <div className={styles['zone-mobile']}>
+              <ul className={styles['zone-logo']}>
+                <li
+                  data-aos="zoom-in-up"
+                  data-aos-duration="500"
+                  data-aos-easing="ease-in-out"
+                  data-aos-once="true">
+                  <div className={styles['img-logotwo'] + ' ' + 'm-auto'}>
+                    <Image src={Ic1} alt="" />
+                  </div>
+                </li>
+                <li
+                  data-aos="zoom-in-up"
+                  data-aos-duration="700"
+                  data-aos-easing="ease-in-out"
+                  data-aos-once="true">
+                  <div className={styles['img-logotwo'] + ' ' + 'm-auto'} style={{ transform: 'translate(0px,10px)' }}>
+                    <Image src={Ic2} alt="" />
+                  </div>
+                </li>
+                <li
+                  data-aos="zoom-in-up"
+                  data-aos-duration="800"
+                  data-aos-easing="ease-in-out"
+                  data-aos-once="true">
+                  <div className={styles['img-logotwo'] + ' ' + 'm-auto'} style={{ transform: 'translate(0px,10px)' }}>
+                    <Image src={Ic3} alt="" />
+                  </div>
+                </li>
+                <li
+                  data-aos="zoom-in-up"
+                  data-aos-duration="900"
+                  data-aos-easing="ease-in-out"
+                  data-aos-once="true">
+                  <div className={styles['img-logotwo'] + ' ' + 'm-auto'}>
+                    <Image src={Ic4} alt="" />
+                  </div>
+                  <div style={{ transform: 'translate(0px,-15px)' }}>
+                    <small >ม.ราชภัฎสงขลา<br />ม.ราชภัฎยะลา</small>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-        </div>
-    
+
       </div>
 
     </Container>
